4f71a7aefbefcf24e7125acb741432738827a067
mo7elessi <mo.t.elessi@gmail.com>
2022-10-13 21:55:16 +0300

    commit

--- lib/presentation/features/editor/pages/trimmer_page.dart
+++ lib/presentation/features/editor/pages/trimmer_page.dart
@@ -12,7 +12,6 @@ import '../../../../data/model/video_model.dart';
 import '../../../widgets/loading_widget.dart';
 import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 
-
 class TrimmerPage extends StatefulWidget {
   final File file;
   final FlagModel flag;
@@ -85,10 +84,8 @@ class _TrimmerPageState extends State<TrimmerPage> {
                     return AlertDialog(
                       title: const Text("select start & end point to mute"),
                       content: Text(
-                        "the default start is the ${startValue ~/
-                            1000}th second and the end is the  ${endTemp ~/
-                            1000}th second\n "
-                            "the numbers interval are chosen from the video tag not the whole video ",
+                        "the default start is the ${startValue ~/ 1000}th second and the end is the  ${endTemp ~/ 1000}th second\n "
+                        "the numbers interval are chosen from the video tag not the whole video ",
                         style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                       actions: [
@@ -106,11 +103,11 @@ class _TrimmerPageState extends State<TrimmerPage> {
                                         value: (StartCurrentValue == 0
                                             ? startValue ~/ 1000
                                             : StartCurrentValue <
-                                            (EndCurrentValue == 0
-                                                ? endTemp ~/ 1000
-                                                : EndCurrentValue)
-                                            ? StartCurrentValue
-                                            : EndCurrentValue - 1),
+                                                    (EndCurrentValue == 0
+                                                        ? endTemp ~/ 1000
+                                                        : EndCurrentValue)
+                                                ? StartCurrentValue
+                                                : EndCurrentValue - 1),
                                         minValue: startValue ~/ 1000,
                                         maxValue: (endTemp ~/ 1000) - 1,
                                         onChanged: (value) {
@@ -128,8 +125,8 @@ class _TrimmerPageState extends State<TrimmerPage> {
                                         itemCount: 3,
                                         value: EndCurrentValue == 0
                                             ? (endValue == 0
-                                            ? endTemp ~/ 1000
-                                            : endValue.toInt())
+                                                ? endTemp ~/ 1000
+                                                : endValue.toInt())
                                             : (EndCurrentValue),
                                         minValue: (startValue ~/ 1000) + 1,
                                         maxValue: endValue == 0
@@ -150,7 +147,8 @@ class _TrimmerPageState extends State<TrimmerPage> {
                                 TextButton(
                                     onPressed: () {
                                       Navigator.pop(context);
-                                    }, child: const Text("ok")),
+                                    },
+                                    child: const Text("ok")),
                               ],
                             )
                           ],
@@ -175,181 +173,172 @@ class _TrimmerPageState extends State<TrimmerPage> {
                 Icons.save,
                 color: !isLoading ? Colors.white : Colors.white60,
               ),
-              onPressed: !isLoading ? () async {
-                final valuee = await getPath();
-                isLoading = true;
-                setState(() {});
-                await trimmer.saveTrimmedVideo(
-                  ffmpegCommand:
-                  "-af \"volume=enable='between(t,${StartCurrentValue -
-                      startValue ~/ 1000},${EndCurrentValue -
-                      startValue ~/ 1000})':volume=0\" "
-                  ,
-                  customVideoFormat: ".mp4",
-                  startValue: startValue,
-                  endValue: endValue == 0 ? endTemp : endValue,
-                  onSave: (String? outputPath) async {
-                    final file = File(outputPath.toString());
-                    String title = widget.flag.title != null ? "${DateTime
-                        .now()
-                        .microsecondsSinceEpoch}_${widget.flag.title}" : DateTime
-                        .now()
-                        .microsecondsSinceEpoch
-                        .toString();
-                    String newPath =
-                        "${valuee.path}/$title.mp4";
-                    await file.copy(newPath);
-                    File(file.path).deleteSync();
-                    VideoModel videoModel = VideoModel(
-                      id: widget.flag.id,
-                      path: newPath,
-                      title: widget.flag.title,
-                      dateTime: DateTime.now(),
-                      videoThumbnail: widget.data.videoThumbnail!,
-                      videoDuration: widget.data.videoDuration!,
-                    );
-                    await Boxes.exportedVideoBox.add(videoModel);
-                    widget.items
-                        .putAt(
-                      widget.videoIndex,
-                      widget.data..flags![widget.flagIndex].isExtracted = true,
-                    )
-                        .then((value) {
-                      File outputPath = File('${valuee.path}/$title.mp4');
-                      String command = '-i $newPath -i $logoPath -filter_complex overlay=10:10 -codec:a copy ${outputPath
-                          .path}';
-                      file.create(recursive: true).then((value) {
-                        FFmpegKit.executeAsync(
-                            command
-                        ).then((session) async {
-                          Navigator.pushNamedAndRemoveUntil(
-                            context,
-                            Routes.homePage,
-                                (route) => false,
+              onPressed: !isLoading
+                  ? () async {
+                      final valuee = await getPath();
+                      isLoading = true;
+                      setState(() {});
+                      await trimmer.saveTrimmedVideo(
+                        ffmpegCommand:
+                            "-af \"volume=enable='between(t,${StartCurrentValue - startValue ~/ 1000},${EndCurrentValue - startValue ~/ 1000})':volume=0\" ",
+                        customVideoFormat: ".mp4",
+                        startValue: startValue,
+                        endValue: endValue == 0 ? endTemp : endValue,
+                        onSave: (String? outputPath) async {
+                          final file = File(outputPath.toString());
+                          String title = widget.flag.title != null
+                              ? "${DateTime.now().microsecondsSinceEpoch}_${widget.flag.title}"
+                              : DateTime.now()
+                                  .microsecondsSinceEpoch
+                                  .toString();
+                          String newPath = "${valuee.path}/$title.mp4";
+                          await file.copy(newPath);
+                          File(file.path).deleteSync();
+                          print("ThE KING");
+                          VideoModel videoModel = VideoModel(
+                            id: widget.flag.id,
+                            path: newPath,
+                            title: widget.flag.title,
+                            dateTime: DateTime.now(),
+                            videoThumbnail: widget.data.videoThumbnail!,
+                            videoDuration: widget.data.videoDuration!,
                           );
-                        }).catchError((error) {});
-                      }).catchError((onError) {
-                        print(onError.toString());
-                      });
-                    });
-                  },
-                );
-              } : null,
+                          await Boxes.exportedVideoBox.add(videoModel);
+                          widget.items
+                              .putAt(
+                            widget.videoIndex,
+                            widget.data
+                              ..flags![widget.flagIndex].isExtracted = true,
+                          )
+                              .then((value) {
+                            File outputPath = File('${valuee.path}/$title.mp4');
+                            String command =
+                                '-i $newPath -i $logoPath -filter_complex overlay=10:10 -codec:a copy ${outputPath.path}';
+                            file.create(recursive: true).then((value) {
+                              FFmpegKit.executeAsync(command)
+                                  .then((session) async {
+                                Navigator.pushNamedAndRemoveUntil(
+                                  context,
+                                  Routes.homePage,
+                                  (route) => false,
+                                );
+                              }).catchError((error) {});
+                            }).catchError((onError) {
+                              print(onError.toString());
+                            });
+                          });
+                        },
+                      );
+                    }
+                  : null,
             ),
           ],
         ),
         body: File(widget.file.path).existsSync() == true
             ? Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              flex: 6,
-              child: Container(
-                color: Colors.black,
-                child: VideoViewer(trimmer: trimmer),
-              ),
-            ),
-            Expanded(
-              child: TrimEditor(
-                trimmer: trimmer,
-                viewerWidth: MediaQuery
-                    .of(context)
-                    .size
-                    .width,
-                onChangeStart: (value) {
-                  setState(() {
-                    startValue = value / 1000;
-                  });
-                },
-                onChangeEnd: (value) {
-                  setState(() {
-                    endValue = value / 1000;
-                    endTemp = endValue;
-                    trimmer.videoPlayerController!.seekTo(
-                        const Duration(seconds: 0));
-                    pausedValue =
-                        trimmer.videoPlayerController!.value.position.inSeconds
-                            .toInt() - 1;
-                  });
-                },
-                onChangePlaybackState: (value) {
-                  setState(() {
-                    _isPlaying = value;
-                  });
-                },
-                flagModel: widget.flag,
-                videoDuration: widget.videoDuration,
-                fit: BoxFit.cover,
-              ),
-            ),
-            StatefulBuilder(
-              builder: (context, setInnerState) {
-                return isLoading ? const LoadingWidget() : Expanded(
-                  child: InkWell(
-                    onTap: () async {
-                      showNumberPickerDialog = true;
-                      await trimmer.videoPlayerController!.pause();
-                    },
-                    child: const Icon(
-                      Icons.music_off_rounded,
-                      color: Colors.yellow,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                      color: Colors.black,
+                      child: VideoViewer(trimmer: trimmer),
                     ),
                   ),
-                );
-              },
-            ),
-            Expanded(
-              child: TextButton(
-                child: _isPlaying
-                    ? const Icon(
-                  Icons.pause,
-                  size: 50.0,
-                  color: Colors.white,
-                )
-                    : const Icon(
-                  Icons.play_arrow,
-                  size: 50.0,
-                  color: Colors.white,
-                ),
-                onPressed: () async {
-                  int duration = trimmer
-                      .videoPlayerController!.value.duration.inSeconds;
-                  if (pausedValue != -1) {
-                    pausedValue = trimmer
-                        .videoPlayerController!.value.position.inSeconds;
-                  }
-                  bool playbackState =
-                  await trimmer.videPlaybackControl(
-                    startValue: ((pausedValue == 0 ||
-                        pausedValue == endTemp ||
-                        pausedValue == endTemp - 1 || pausedValue == -1)
-                        ? (startValue ~/ 1000)
-                        : (pausedValue)) *
-                        1000,
-                    endValue: endValue,
-                  );
-                  setState(() {
-                    _isPlaying = playbackState;
-                    pausedValue = trimmer
-                        .videoPlayerController!.value.position.inSeconds;
-                  });
-                },
-              ),
-            )
-          ],
-        )
+                  Expanded(
+                    child: TrimEditor(
+                      trimmer: trimmer,
+                      viewerWidth: MediaQuery.of(context).size.width,
+                      onChangeStart: (value) {
+                        setState(() {
+                          startValue = value / 1000;
+                        });
+                      },
+                      onChangeEnd: (value) {
+                        setState(() {
+                          endValue = value / 1000;
+                          endTemp = endValue;
+                          trimmer.videoPlayerController!
+                              .seekTo(const Duration(seconds: 0));
+                          pausedValue = trimmer.videoPlayerController!.value
+                                  .position.inSeconds
+                                  .toInt() -
+                              1;
+                        });
+                      },
+                      onChangePlaybackState: (value) {
+                        setState(() {
+                          _isPlaying = value;
+                        });
+                      },
+                      flagModel: widget.flag,
+                      videoDuration: widget.videoDuration,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  StatefulBuilder(
+                    builder: (context, setInnerState) {
+                      return isLoading
+                          ? const LoadingWidget()
+                          : Expanded(
+                              child: InkWell(
+                                onTap: () async {
+                                  showNumberPickerDialog = true;
+                                  await trimmer.videoPlayerController!.pause();
+                                },
+                                child: const Icon(
+                                  Icons.music_off_rounded,
+                                  color: Colors.yellow,
+                                ),
+                              ),
+                            );
+                    },
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      child: _isPlaying
+                          ? const Icon(
+                              Icons.pause,
+                              size: 50.0,
+                              color: Colors.white,
+                            )
+                          : const Icon(
+                              Icons.play_arrow,
+                              size: 50.0,
+                              color: Colors.white,
+                            ),
+                      onPressed: () async {
+                        int duration = trimmer
+                            .videoPlayerController!.value.duration.inSeconds;
+                        if (pausedValue != -1) {
+                          pausedValue = trimmer
+                              .videoPlayerController!.value.position.inSeconds;
+                        }
+                        bool playbackState = await trimmer.videPlaybackControl(
+                          startValue: ((pausedValue == 0 ||
+                                      pausedValue == endTemp ||
+                                      pausedValue == endTemp - 1 ||
+                                      pausedValue == -1)
+                                  ? (startValue ~/ 1000)
+                                  : (pausedValue)) *
+                              1000,
+                          endValue: endValue,
+                        );
+                        setState(() {
+                          _isPlaying = playbackState;
+                          pausedValue = trimmer
+                              .videoPlayerController!.value.position.inSeconds;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              )
             : const Center(
-          child: Text("Unknown video"),
-        ),
+                child: Text("Unknown video"),
+              ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
